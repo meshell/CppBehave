760809c9655e208e8b5a11d603566913b794aaa7
Michel Estermann <estermann.michel@gmail.com>
2017-01-06 23:45:45 +0100

    First example implementation.

---
+++ example.cpp
@@ -0,0 +1,42 @@
+#include "cpp_behave/test_step.h"
+
+#include <memory>
+
+
+// Example
+
+class Calculator
+{
+public:
+    template <typename T>
+    T add(T rhs, T lhs)
+    {
+        return rhs + lhs;
+    }
+};
+
+using namespace cpp_behave;
+
+namespace {
+
+    SPEC(A_Calculator, should_add_correctly)
+    {
+        const auto x = 3;
+        const auto y = 6;
+        auto result = 0;
+        const auto expected_res = 9;
+
+        std::unique_ptr<Calculator> calculator;
+
+        "Given a Calculator"_
+                .f([&]{ calculator = std::unique_ptr<Calculator>(new Calculator{});});
+        "and the Number %"_
+                .f([](const auto){}, x );
+        "and the Number %"_
+                .f([&](const auto){}, y);
+        "When I add the numbers together"_
+                .f([&](){ result = calculator->add(x, y);});
+        "Then the result should be %"_
+                .f([&](const auto expected_result){ ASSERT_EQ(expected_result, result); }, expected_res);
+    }
+}
---
+++ include/cpp_behave/test_step.h
@@ -0,0 +1,56 @@
+#ifndef CPPBEHAVE_TEST_STEP_H_H
+#define CPPBEHAVE_TEST_STEP_H_H
+
+#include <gtest/gtest.h>
+#include <iostream>
+
+#define SPEC(test_case_name, test_name) GTEST_TEST(test_case_name, test_name)
+
+
+namespace {
+    static void tprintf(const char *format) {
+        std::cout << format << "\n";
+    }
+
+    template<typename T, typename ...Targs>
+    static void tprintf(const char *format, T value, Targs... args) {
+        for (; *format != '\0'; format++) {
+            if (*format == '%') {
+                std::cout << value;
+                tprintf(format + 1, args...);
+                return;
+            }
+            std::cout << *format;
+        }
+        std::cout << "\n";
+    };
+}
+
+namespace cpp_behave
+{
+        class test_step {
+            public:
+            test_step() = default;
+
+            explicit test_step(const std::string &step_text)
+                    : step_text_(step_text) {}
+
+            template<typename T, typename ...Targs>
+            void f(const T &fnc, Targs &&... args) {
+                tprintf(step_text_.c_str(), std::forward<Targs>(args)...);
+                fnc(std::forward<Targs>(args)...);
+            }
+
+            private:
+            std::string step_text_;
+
+        };
+
+
+        static test_step operator "" _ ( const char* text, std::size_t)
+        {
+            return test_step(text);
+        }
+}
+
+#endif //CPPBEHAVE_TEST_STEP_H_H
